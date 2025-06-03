ca3c21edf419a72cc220631b8f78afb931effeb4
unknown <ericsmk12@gmail.com>
2025-06-03 10:24:08 +0700

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
+
 import './App.css';
 
+import { useEffect, useState } from 'react';
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [photos, setPhotos] = useState([]);
+
+    useEffect(() => {
+        fetch('https:/jsonplaceholder.typicode.com/photos?_limit=5')
+            .then(response => response.json())
+            .then(data => setPhotos(data));
+    }, []);
+
+    return (
+        <div>
+            <h1>Galeri Foto</h1>
+            <ul>
+                {photos.map(photo => (
+                    <li key= { photo.id }>
+                        <img src={ photo.thumnailUrl } alt = { photo.title } width = "150" />
+                        <p>{ photo.title }</p>
+                    </li>
+                ))}
+            </ul>
+        </div>
+    )
 }
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders heading Galeri Foto', () => {
+    render(<App />);
+    const headingElement = screen.getByText(/Galeri Foto/i);
+    expect(headingElement).toBeInTheDocument();
 });
